1265bfad30920bdaffbf4b7653caed8b0f43ede0
Sisyetad <sisaytadewos@gmail.com>
2026-02-19 11:09:39 +0300

    fixing cors related issue

--- optimalroute/config/settings.py
+++ optimalroute/config/settings.py
@@ -50,9 +50,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'infrastructure',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -102,7 +104,7 @@ CACHES = {
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = False
 
 REST_FRAMEWORK = {
     'DEFAULT_THROTTLE_CLASSES': [
